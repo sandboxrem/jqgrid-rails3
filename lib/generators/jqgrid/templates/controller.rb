a3d0571593a8bc247e7e9d2784a4bcc7ea7c02f6
Dave Baldwin <dave.baldwin@dsl.pipex.com>
2011-07-01 12:51:35 +0100

    Updated generator to support master-detail views.

--- lib/generators/jqgrid/templates/controller.rb
+++ lib/generators/jqgrid/templates/controller.rb
@@ -6,6 +6,8 @@ class <%= class_name.pluralize %>Controller < ApplicationController
 	#resources :<%=plural_name%>,:only => [:index] do
 	#	collection do
 	#	  post "post_data"
+<%=details.map {|detail| "\t#     post \"#{detail.model.downcase}_post_data\""}.join("\n")%>
+<%=details.map {|detail| "\t#     get \"#{detail.model.downcase}_details\""}.join("\n")%>
 	#	end
 	# end
 
@@ -15,10 +17,25 @@ class <%= class_name.pluralize %>Controller < ApplicationController
 		grid_add_edit_del(<%=class_name%>, GRID_COLUMNS)
 	end
 	
-	
 	def index
 		respond_with() do |format|
 			format.json { render :json => filter_on_params(<%=class_name%>, GRID_COLUMNS)}  
 		end
 	end
+	
+	<% details.each do |detail| %>
+	<%=detail.model.upcase%>_GRID_COLUMNS = %w{<%= detail.columns.map {|x| "#{x}"}.join(' ') %>}
+
+	def <%=detail.model.downcase%>_post_data
+		grid_add_edit_del(<%=detail.model%>, <%=detail.model.upcase%>_GRID_COLUMNS)
+	end
+
+	def <%=detail.model.downcase%>_details
+		respond_with() do |format|
+			format.json { render :json => filter_details(<%=class_name%>, '<%=detail.foreign_key%>', <%=detail.model%>, <%=detail.model.upcase%>_GRID_COLUMNS)}  
+		end
+	end
+	
+	
+	<% end %>
 end
