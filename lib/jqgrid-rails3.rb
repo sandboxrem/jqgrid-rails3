481b38acc5ce52b7b43bfc08b12de905636b9fb6
Rustam Mamedov <KharkivReM@gmail.com>
2012-04-02 00:08:08 -0500

    fixed syntax error

--- lib/jqgrid-rails3.rb
+++ lib/jqgrid-rails3.rb
@@ -2,7 +2,7 @@ require "jqgrid-rails3/version"
 
 module Jqgrid
   class Railtie < Rails::Railtie
-    railtie_name :jqgrid-rails3
+    railtie_name 'jqgrid-rails3'
 
     rake_tasks do
       load 'tasks/2dc_jqgrid_tasks.rake'
